96712bdcec13ac424b6b6fc5d3bc1b89a5fd985f
Segun Ajibola <ajbl.segun@gmail.com>
2022-04-08 23:16:31 +0100

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-let firstCard = 7
-let secondCard = 7
-
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.get
 
 console.log(sum)
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card?")
+} else if (sum === 21) {
+    console.log("Wahoo! You've got blackjack")
+} else {
+    console.log("You're out of the game!")
+    isAlive = false
+}
+
+console.log(isAlive)

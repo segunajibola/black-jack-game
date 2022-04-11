6c5eaffe8260437a2dca640d655a4648deed7a4a
Segun Ajibola <ajbl.segun@gmail.com>
2022-04-11 10:09:49 +0100

    first commit

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
+let player = {
+    name: "Segun",
+    chips: 200
+}
+
 let cards = []
 let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-
-let player = {
-    name: "John",
-    chips: 145
-}
 
 let playerEl = document.getElementById("player-el")
 playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard() {
     let randomNumber = Math.floor ( Math.random() * 13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
 }
 
 function startGame() {
